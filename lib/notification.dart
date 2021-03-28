d3f2c250bfdcb6eea39dd9ac3ce51835be9aa6f2
aniketc12 <aniketc12@outlook.com>
2021-03-28 00:01:52 -0700

    Notification system

--- lib/notification.dart
+++ lib/notification.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class FirebaseNotification {
 
@@ -11,23 +12,50 @@ class FirebaseNotification {
         print("Handling a background message: ${message.messageId}");
     }
 
+
     FirebaseNotification();
 
     void init() async{
         await Firebase.initializeApp();
+
+
+
+
+        FlutterLocalNotificationsPlugin local_not = new FlutterLocalNotificationsPlugin();
+        final androidSpec = AndroidNotificationDetails(
+                'channel_id',
+                'channel_name',
+                'channel_description',
+                importance: Importance.max,
+                priority: Priority.high,
+                ticker: 'test ticker',
+        );
+
+
+        final iosSpec = IOSNotificationDetails();
+        final androidSettings = AndroidInitializationSettings('app_icon');
+        final iosSettings = IOSInitializationSettings(onDidReceiveLocalNotification: (int id, 
+                        String title, String body, String payload){});
+        final settings = InitializationSettings(android: androidSettings, iOS: iosSettings);
+        local_not.initialize(settings, onSelectNotification: (String payload) {
+        });
+        final spec = NotificationDetails(android: androidSpec, iOS: iosSpec);
+
         FirebaseMessaging messaging = FirebaseMessaging.instance;
         messaging.getToken().then((token) {
             print("**************\n");
             print(token);
         });
-        FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-        FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-            print('Got a message whilst in the foreground!');
-            print('Message data: ${message.data}');
 
-            if (message.notification != null) {
-                print('Message also contained a notification: ${message.notification}');
-            }
+        FirebaseMessaging.onBackgroundMessage((RemoteMessage message) async {
+            await Firebase.initializeApp();
+            await local_not.show(1, "Test Title", message.notification.body, spec, payload: "payload");
         });
+
+        FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+            print('Got a message whilst in the foreground!');
+            await local_not.show(1, message.notification.title, message.notification.body, spec, payload: "payload");
+        });
+
     }
 }
