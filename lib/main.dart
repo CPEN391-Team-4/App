b4d3c0130f32bcd6c17e8dbbfd4fd961a14924d2
aniketc12 <aniketc12@outlook.com>
2021-04-01 16:13:18 -0700

    Pre-Merge

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,52 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'notification.dart';
 import 'package:get/get.dart';
+import 'home_widget.dart';
+import 'dart:math';
 
 
 
 
 void main() async{
     WidgetsFlutterBinding.ensureInitialized();
-    // final notification = FirebaseNotification();
-//    notification.init();
     
     runApp(GetMaterialApp(home: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+    @override
+    State<StatefulWidget> createState() {
+        return _MyAppState();
+    }
+}
+
+class _MyAppState extends State<MyApp> {
+    bool check = true;
+    @override
+    void initState() {
+        setCheck();
+    }
+
+    Future<bool> setCheck() async {
+        FirebaseMessaging.onMessageOpenedApp.listen((message) async {
+            WidgetsFlutterBinding.ensureInitialized();
+        });
+        await Future.delayed(Duration(seconds: 6));
+        Get.off(Home(false,0));
+    }
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // home: Home(),
-            home: Greet(),
-    );
+      if (check == true) {
+          return MaterialApp(
+                  home:Greet(),
+          );
+
+      }
+      else {
+          return MaterialApp(
+                  home:Home(false,0),
+          );
+      }
   }
 }
