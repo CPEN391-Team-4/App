45db40259d151e872b0f542e33bd92d8031daabe
Brendonchenhao <Suneduxch@gmail.com>
2021-04-07 17:30:15 -0700

    streamtesting

--- lib/connect.dart
+++ lib/connect.dart
@@ -1,6 +1,7 @@
 import 'package:grpc/grpc.dart';
 import 'package:my_app/protobuf/TrustPeople.pb.dart';
 import 'package:my_app/protobuf/TrustPeople.pbgrpc.dart';
+import 'package:my_app/protobuf/video.pbgrpc.dart';
 import 'protobuf/TrustPeople.pb.dart';
 import 'protobuf/TrustPeople.pbgrpc.dart';
 
@@ -13,5 +14,16 @@ connectStart() {
   print("Connect");
   final stub = RouteClient(channel,
       options: CallOptions(timeout: Duration(seconds: 20)));
+  return [stub, channel];
+}
+
+connectStartvideo() {
+  final channel = ClientChannel('192.168.0.101',
+      port: 9000,
+      options:
+          const ChannelOptions(credentials: ChannelCredentials.insecure()));
+
+  print("Connect");
+  final stub = VideoRouteClient(channel);
   return [stub, channel];
 }
