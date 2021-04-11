6bf386561b14e3ee6d90273e583cd089ad492d3e
Brendonchenhao <Suneduxch@gmail.com>
2021-04-10 17:47:28 -0700

    update

--- lib/newBluetooth.dart
+++ lib/newBluetooth.dart
@@ -5,146 +5,168 @@ import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'profile.dart';
-
+import 'protobuf/TrustPeople.pb.dart';
+import 'protobuf/TrustPeople.pbgrpc.dart';
+import 'package:grpc/grpc.dart';
+import 'eachTrustedUser.dart';
+import 'connect.dart';
 
 class FindDevices extends StatefulWidget {
-    @override
-    _FindDevicesState createState() => new _FindDevicesState();
+  @override
+  _FindDevicesState createState() => new _FindDevicesState();
 }
 
-
 class _FindDevicesState extends State<FindDevices> {
-    List<BluetoothDiscoveryResult> devicesList = List<BluetoothDiscoveryResult>();
-    StreamSubscription<BluetoothDiscoveryResult> deviceSubscription;
+  List<BluetoothDiscoveryResult> devicesList = List<BluetoothDiscoveryResult>();
+  StreamSubscription<BluetoothDiscoveryResult> deviceSubscription;
 
-    @override
-    void initState() {
-        startBluetooth();
-    }
+  @override
+  void initState() {
+    startBluetooth();
+  }
 
-    void startBluetooth() {
-        deviceSubscription = FlutterBluetoothSerial.instance.startDiscovery().listen((r) {
-            if (r.device.name == "FaceLock" && devicesList.every((element) => element.device.address != r.device.address)) {
-                setState(() {
-                   devicesList.add(r);
-                });
-            }
-        });
-    }
-
-    void refresh() {
+  void startBluetooth() {
+    deviceSubscription =
+        FlutterBluetoothSerial.instance.startDiscovery().listen((r) {
+      if (r.device.name == "FaceLock" &&
+          devicesList
+              .every((element) => element.device.address != r.device.address)) {
         setState(() {
-            devicesList.clear();
+          devicesList.add(r);
         });
-        startBluetooth();
-    }
+      }
+    });
+  }
 
-    @override
-    void dispose() {
-        deviceSubscription.cancel();
-        super.dispose();
-    }
+  void refresh() {
+    setState(() {
+      devicesList.clear();
+    });
+    startBluetooth();
+  }
 
-    Future<void> _alert(context, string1, string2) async {
-        return showDialog<void>(
-                context: context,
-                barrierDismissible: false, // user must tap button!
-                builder: (BuildContext context) {
-                    return AlertDialog(
-                            content: SingleChildScrollView(
-                                    child: ListBody(
-                                            children: <Widget>[
-                                                Text(string1),
-                                                Text(string2),
-                                            ],
-                                    ),
-                            ),
-                            actions: <Widget>[
-                                TextButton(
-                                        child: Text('Ok'),
-                                        onPressed: () {
-                                            Navigator.pop(context);
-                                        },
-                                ),
-                            ],
-                    );
-                },
-                );
-    }
+  @override
+  void dispose() {
+    deviceSubscription.cancel();
+    super.dispose();
+  }
 
-    ListView _buildListViewOfDevices() {
-        List<Container> containers = new List<Container>();
-        for (BluetoothDiscoveryResult device in devicesList) {
-            containers.add(
-                    Container(
-                            height: 70,
-                            child: Row(
-                                    children: <Widget>[
-                                        Expanded(
-                                                child: Column(
-                                                        children: <Widget>[
-                                                            Text(device.device.name == null ? '(unknown device)' : device.device.name),
-                                                            Text(device.device.address.toString()),
-                                                        ],
-                                                ),
-                                        ),
-                                        FlatButton(
-                                                color: Colors.blue,
-                                                child: Text(
-                                                        'Connect',
-                                                        style: TextStyle(color: Colors.white),
-                                                ),
-                                                onPressed: () async {
-                                                    BluetoothConnection connection = await BluetoothConnection.toAddress(device.device.address);
-                                                    print("Connected to "+device.device.name);
-                                                    var send = utf8.encode("devid\r\n"); //Convert string to bytes
-                                                    var deviceID = BytesBuilder();
-                                                    connection.input.listen((data) async {
-                                                        deviceID.add(data);
-                                                        if (deviceID.toBytes()[deviceID.length-1] == 10) {
-                                                            print(deviceID.toBytes().toString());
-                                                            _sendIDtoBackend(deviceID.toBytes().toString());
-                                                            await connection.close();
-                                                            await connection.finish();
-                                                            connection.dispose();
-                                                            deviceSubscription.cancel();
-                                                            _alert(context, "Successfully connected device", "");
-                                                            refresh();
-                                                            Get.off(Profile());
-                                                        }
-                                                    }).onDone(() {
-                                                    });
-
-                                                    connection.output.add(send); 
-                                                }
+  Future<void> _alert(context, string1, string2) async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                Text(string1),
+                Text(string2),
+              ],
             ),
-            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Ok'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
             ),
-            ),
-            );
-        }
-
-        return ListView(
-                padding: const EdgeInsets.all(8),
-                children: <Widget>[
-                    ...containers,
-                ],
+          ],
         );
-    }
+      },
+    );
+  }
 
-    void _sendIDtoBackend(id) {
-        // Make GRPC call
-
-    }
-
-
-    @override 
-    Widget build(context) {
-        return Scaffold(
-                appBar: AppBar(
-                        title: Text("Connect to Security Device"),
+  ListView _buildListViewOfDevices() {
+    List<Container> containers = new List<Container>();
+    for (BluetoothDiscoveryResult device in devicesList) {
+      containers.add(
+        Container(
+          height: 70,
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Column(
+                  children: <Widget>[
+                    Text(device.device.name == null
+                        ? '(unknown device)'
+                        : device.device.name),
+                    Text(device.device.address.toString()),
+                  ],
                 ),
-                body: _buildListViewOfDevices(),
-        );
+              ),
+              FlatButton(
+                  color: Colors.blue,
+                  child: Text(
+                    'Connect',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () async {
+                    BluetoothConnection connection =
+                        await BluetoothConnection.toAddress(
+                            device.device.address);
+                    print("Connected to " + device.device.name);
+                    var send =
+                        utf8.encode("devid\r\n"); //Convert string to bytes
+                    var deviceID = BytesBuilder();
+                    connection.input.listen((data) async {
+                      deviceID.add(data);
+                      if (deviceID.toBytes()[deviceID.length - 1] == 10) {
+                        print(deviceID.toBytes().toString());
+                        _sendIDtoBackend(deviceID.toBytes().toString());
+                        await connection.close();
+                        await connection.finish();
+                        connection.dispose();
+                        deviceSubscription.cancel();
+                        _alert(context, "Successfully connected device", "");
+                        refresh();
+                        Get.off(Profile());
+                      }
+                    }).onDone(() {});
+
+                    connection.output.add(send);
+                  }),
+            ],
+          ),
+        ),
+      );
     }
+
+    return ListView(
+      padding: const EdgeInsets.all(8),
+      children: <Widget>[
+        ...containers,
+      ],
+    );
+  }
+
+  Future<void> _sendIDtoBackend(id) async {
+    // Make GRPC call
+    print("send de1 id to server");
+    final ret = await connectStart(10);
+    var stub = ret[0];
+    var channel = ret[1];
+
+    try {
+      var username = "admin";
+      var de1info = BluetoothInfo()..de1ID = id;
+      var res = await stub.sendDe1ID(de1info);
+    } catch (e) {
+      print(e);
+      await channel.shutdown();
+      return 
+    }
+    await channel.shutdown();
+  }
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Connect to Security Device"),
+      ),
+      body: _buildListViewOfDevices(),
+    );
+  }
 }
